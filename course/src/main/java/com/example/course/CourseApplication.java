4b62f12ea050e5b95a9245d22807a2a8cc87a8cb
ManojM129 <717822f129@kce.ac.in>
2025-07-29 09:57:51 +0530

    Initial package

---
+++ course/src/main/java/com/example/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+//	System.out.println("hello");
+}
